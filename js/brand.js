b8341fbb847f09c8ed2b7ba3b5e574854bfa7cd3
jieunChoi12 <cje912@naver.com>
2022-06-03 16:34:34 +0900

    보충 업로드

--- js/brand.js
+++ js/brand.js
@@ -1,5 +1,16 @@
 $(function(){
 
+    $('.brand .tab li:first-child').click(function(e){
+        e.preventDefault();
+        $('main section').show();
+    });
+    $('.brand .tab li a').click(function(e){
+        e.preventDefault();
+        $('main section').hide();
+        $($(this).attr('href')).show();
+    });
+
+
      //탑버튼
      let top = $('.top a');
 
@@ -14,4 +25,5 @@ $(function(){
          e.preventDefault();
          $('body,html').animate({scrollTop:0},500);
      });
+
 });
--- js/story.js
+++ js/story.js
@@ -66,10 +66,10 @@ $(function(){
     //호버써클
     let li = $('.story_2 ul li');
     li.mouseover(function(){
-        $(this).find('circle').stop().animate({strokeDashoffset:0},500);
+        $(this).find('circle').stop().animate({strokeDashoffset:0},400);
     });
     li.mouseout(function(){
-        $(this).find('circle').stop().animate({strokeDashoffset:1118},500);
+        $(this).find('circle').stop().animate({strokeDashoffset:1118},400);
     });
 
     //풀페이지
